6fdc64b41d7f40b66d5a5933eb8013d510f0efc9
609529897 <609529897@qq.com>
2020-06-03 11:19:54 +0800

    depley

---
+++ precache-manifest.5adad5e2b23416afed4bef26246b2980.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6681973cf81d0620d270d76a0c1777da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55990185518a25f081ce",
+    "url": "/static/js/2.d6d0d712.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.d6d0d712.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5f84aafb84c0f7d121c",
+    "url": "/static/js/main.dce5e334.chunk.js"
+  },
+  {
+    "revision": "a7974c852811458cb314",
+    "url": "/static/js/runtime-main.7908c26f.js"
+  }
+]);
